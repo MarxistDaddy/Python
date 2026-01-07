31d8922838352c2402c779aa015868ccbb8f07a7
Hamza Aarab <hamaarab@c3r4p5.1337.ma>
2026-01-07 17:10:57 +0100

    working on 01 and 02 to push them

--- module_01/ex0/ft_garden_intro.py
+++ module_01/ex0/ft_garden_intro.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3.10
 
+"""
+this program is about knowing how the module is being executed by relying on __name__ variable
+
+a python module can either be run directly or imported!
+__name__ in python, the special module variable, is a special bulit-in variable that exists in every Python module. its automatically set by Python and its value depends on how the module is being executed.
+
+
+
+
+
+
+
+"""
+
 if __name__ == "__main__":
     print("=== Welcome to My Garden ===")
     print("Plant: Rose")
     print("Height: 25cm")
     print("Age: 30 days\n")
-    print("=== End of Program ===")
+    print("=== End of Program ===\n\n")
